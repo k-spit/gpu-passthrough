61a84ef00d297dce6f5654154d0555c7736e5a9b
k-spit <kspit1991@gmail.com>
2019-11-23 23:59:28 +0100

    make more generic

--- usb-focusrite/attach-detach.sh
+++ usb-focusrite/attach-detach.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-SomeString="1235"
+vendorid="0x1235"
+domain="win10"
+devicedesc=/home/desktop/gpu-passthrough/usb-focusrite/usb-focusrite.xml
 
-virsh dumpxml win10 > tmp
-if grep -q $SomeString tmp; then
-  echo -e "found"
-  virsh detach-device win10 --file /home/desktop/gpu-passthrough/usb-focusrite/usb-focusrite.xml
+virsh dumpxml $domain > tmp
+if grep -q $vendorid tmp; then
+  echo -e "vendorid: $vendorid found"
+  virsh detach-device $domain --file $devicedesc
   exit
 else
-  echo -e "not found"
-  virsh attach-device win10 --file /home/desktop/gpu-passthrough/usb-focusrite/usb-focusrite.xml
+  echo -e "no vendorid found"
+  virsh attach-device $domain --file $devicedesc
   exit
 fi
